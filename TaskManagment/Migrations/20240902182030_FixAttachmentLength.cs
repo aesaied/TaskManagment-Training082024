f929235bc6b724ba0598be730d8331dbf2daae2d
Atallah Esaied <atallah.esaied@gmail.com>
2024-09-02 21:21:47 +0300

    Fix Attachment Length

---
+++ TaskManagment/Migrations/20240902182030_FixAttachmentLength.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManagment.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixAttachmentLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "ContentLength",
+                table: "Attachments",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ContentLength",
+                table: "Attachments",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
